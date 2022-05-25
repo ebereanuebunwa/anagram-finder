03bd5c90a2f1823f47124fcfc4e3adec80997b89
ebereanuebunwa <70179219+ebereanuebunwa@users.noreply.github.com>
2022-05-25 14:57:49 +0100

    Update file.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # Check if two words are anagrams 
 
-first_word = input('First word: ')
-second_word = input('Second word: ')
-
 
 def find_anagram(word, anagram):
     # for two words to be anagrams, they must meet certain criteria:
     # 1. They must have the same set of letters.
-    # 2. Each letter is positioned differently in each word.
+    # 2. Each letter is positioned differently in both words.
     # 3. Both words are equal in length.
 
-    # append all the letters in word and anagram to two separate lists.
+    # Append all the letters in word and anagram to two separate lists.
+    # An anagram could be a word or a phrase.
+
     word_list = [letter.lower() for letter in word if letter != ' ']
     anagram_list = [letter.lower() for letter in anagram if letter != ' ']
 
@@ -18,16 +17,10 @@ def find_anagram(word, anagram):
     sorted_word_list = sorted(word_list)
     sorted_anagram_list = sorted(anagram_list)
 
-    # if both lists are not equal after sorting, then the two words 
-    # are not anagrams.
+    # if both lists are not equal after sorting, then the two words are not anagrams.
     # Use an if-statement
     if sorted_word_list != sorted_anagram_list:
         return False
 
     return True
 
-
-if find_anagram(first_word, second_word):
-    print('Both words are anagrams.')
-else:
-    print('They are not anagrams.')
